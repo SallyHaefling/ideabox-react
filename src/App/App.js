3b2beb31a1d0ed6f3b27f821b8ee4c30e1a4014f
Sally Haefling <sallyhaefling@gmail.com>
2019-04-22 14:53:48 -0600

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import IdeaForm from '../IdeaForm/IdeaForm';
+import IdeaContainer from '../IdeaContainer/IdeaContainer'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ideas: []
+    }
+  }
+
+  removeIdea = (id) => {
+    const newIdeas = this.state.ideas.filter(idea => idea.id !== id)
+    this.setState({ideas: newIdeas})
+  }
+
+  addIdea = (idea) => {
+    const newIdeas = [...this.state.ideas, {title: idea.title, body: idea.body, id: Date.now()}]
+    this.setState({ ideas: newIdeas })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h2>IdeaBox</h2>
+       <IdeaForm addIdea={this.addIdea}/>
+       <IdeaContainer ideas={this.state.ideas} removeIdea={this.removeIdea}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/IdeaContainer/IdeaContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import IdeaCard from '../IdeaCard/IdeaCard';
+
+const IdeaContainer = (props) => {
+  const ideaCards = props.ideas.map(idea => {
+    return <IdeaCard 
+    key={idea.id}
+    idea={idea}
+    removeIdea={props.removeIdea} />
+  })
+  return <div>
+    {ideaCards}
+  </div>
+}
+
+export default IdeaContainer;
