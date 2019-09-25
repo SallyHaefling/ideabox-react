cf2e0d91e48add2e7cfebd6dad0bc4041df52275
Sally Haefling <sallyhaefling@gmail.com>
2019-09-25 13:59:10 -0600

    Restart ideabox

--- src/App/App.js
+++ src/App/App.js
@@ -1,40 +1,25 @@
 import React, {Component} from 'react';
-import '../App.css';
-import IdeaForm from '../IdeaForm/IdeaForm';
-import IdeaContainer from '../IdeaContainer/IdeaContainer';
+// import IdeaCard from './IdeaCard';
+// import IdeaContainer from './IdeaContainer';
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state={
       ideas: []
     }
   }
 
-  addIdea = (idea) => {
-    const newIdeas = [...this.state.ideas, 
-      {title: idea.title, body: idea.body, id: Date.now()}];
-    this.setState({
-      ideas: newIdeas
-    })
-  }
-
-  removeIdea = (id) => {
-    const newIdeas = this.state.ideas.filter(idea => idea.id !== id);
-    this.setState({
-      ideas: newIdeas
-    })
-  }
+addIdea = (idea) => {
+  const newIdea = {title: idea, body: idea.body, id: Date.now()};
+  this.props.createIdea(newIdea);
+}
 
   render() {
-    return (
-      <section className = 'App'>
-        <h2>IdeaBox in React</h2>
-        <IdeaForm addIdea={this.addIdea}/>
-        <IdeaContainer removeIdea={this.removeIdea}
-                       ideas={this.state.ideas}
-        />
-      </section>
+    return(
+      <div>
+        <h1>IdeaBox</h1>
+      </div>
     )
   }
 }
